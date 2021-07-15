de81e4c9d6a185a34d071c896cc02418d99ad241
DocFu <arne.schueler@gmail.com>
2021-07-15 03:05:38 +0200

    Improved logging Implemented socket sending and receiving Added reconnect after web socket fail

--- src/main/java/de/plasmawolke/qlcplusbridge/qlc/VirtualConsoleButton.java
+++ src/main/java/de/plasmawolke/qlcplusbridge/qlc/VirtualConsoleButton.java
@@ -5,16 +5,25 @@ import io.github.hapjava.characteristics.HomekitCharacteristicChangeCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.beans.VetoableChangeListener;
+import java.beans.VetoableChangeSupport;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class VirtualConsoleButton implements LightbulbAccessory {
 
 
+    public static final String CHANGE_PROPERTY_NAME = "VirtualConsoleButtonState";
     private static final Logger logger = LoggerFactory.getLogger(VirtualConsoleButton.class);
+    private VetoableChangeSupport propertyChangeSupport = new VetoableChangeSupport(this);
+
+    private HomekitCharacteristicChangeCallback subscribeCallback = null;
 
     private int accessoryId;
 
-    private int htmlId;
+    private int qlcId;
     private String name;
 
     private String serialNumber = "1234-5678"; // TODO
@@ -22,14 +31,14 @@ public class VirtualConsoleButton implements LightbulbAccessory {
     private String manufacturer = "QLC+ Bridge";
     private String firmwareRevision = "0.1"; // TODO
 
-    private boolean state;
+    private AtomicBoolean state = new AtomicBoolean(false);
 
-    public VirtualConsoleButton(int htmlId, String name) {
+    public VirtualConsoleButton(int qlcId, String name) {
 
-        if (htmlId < 0) {
-            throw new IllegalArgumentException("'htmlId' must be greater than 0.");
+        if (qlcId < 0) {
+            throw new IllegalArgumentException("'qlcId' must be greater than 0.");
         }
-        this.htmlId = htmlId;
+        this.qlcId = qlcId;
 
         if (name == null || name.length() == 0) {
             throw new IllegalArgumentException("'name' may not be empty.");
@@ -40,14 +49,23 @@ public class VirtualConsoleButton implements LightbulbAccessory {
 
     }
 
-    public VirtualConsoleButton(int htmlId, String name, boolean initialState) {
-        this(htmlId,name);
-        state = initialState;
+    public VirtualConsoleButton(int qlcId, String name, boolean initialState) {
+        this(qlcId, name);
+        state = new AtomicBoolean(initialState);
     }
 
-    private void createAndSetAccessoryId(){
-        accessoryId = (htmlId + name).hashCode();
-        if(accessoryId < 0){
+    public void addPropertyChangeListener(VetoableChangeListener pcl) {
+        propertyChangeSupport.addVetoableChangeListener(pcl);
+    }
+
+    public void removePropertyChangeListener(VetoableChangeListener pcl) {
+        propertyChangeSupport.removeVetoableChangeListener(pcl);
+    }
+
+
+    private void createAndSetAccessoryId() {
+        accessoryId = (qlcId + name).hashCode();
+        if (accessoryId < 0) {
             accessoryId = accessoryId * -1;
         }
     }
@@ -55,24 +73,56 @@ public class VirtualConsoleButton implements LightbulbAccessory {
 
     @Override
     public CompletableFuture<Boolean> getLightbulbPowerState() {
-        return CompletableFuture.completedFuture(state);
+        return CompletableFuture.completedFuture(state.get());
     }
 
     @Override
-    public CompletableFuture<Void> setLightbulbPowerState(boolean powerState) throws Exception {
-        state = powerState;
-        logger.info("Changed power state for "+ this.toString()+".");
+    public CompletableFuture<Void> setLightbulbPowerState(boolean newState) throws Exception {
+
+
+        if (state.compareAndSet(!newState, newState)) {
+            if (subscribeCallback != null) {
+                subscribeCallback.changed();
+            }
+
+            try{
+                propertyChangeSupport.fireVetoableChange(CHANGE_PROPERTY_NAME, !newState, newState);
+                logger.info("Homekit: '" + name + "' --> " + (newState ? "ON" : "OFF") + "");
+            } catch (Exception e){
+                logger.error(e.getMessage());
+                throw new Exception(e.getMessage());
+            }
+
+        }
+
+        return CompletableFuture.completedFuture(null);
+    }
+
+    /**
+     * Additional methode without firing a property change.
+     *
+     * @param newState
+     * @return
+     * @throws Exception
+     */
+    public CompletableFuture<Void> setLightbulbPowerStateFromSocket(boolean newState) throws Exception {
+        if (state.compareAndSet(!newState, newState)) {
+            if (subscribeCallback != null) {
+                subscribeCallback.changed();
+            }
+            logger.info("QLC+: '" + name + "' --> " + (newState ? "ON" : "OFF") + "");
+        }
         return CompletableFuture.completedFuture(null);
     }
 
     @Override
     public void subscribeLightbulbPowerState(HomekitCharacteristicChangeCallback callback) {
-
+        this.subscribeCallback = callback;
     }
 
     @Override
     public void unsubscribeLightbulbPowerState() {
-
+        this.subscribeCallback = null;
     }
 
     @Override
@@ -87,7 +137,7 @@ public class VirtualConsoleButton implements LightbulbAccessory {
 
     @Override
     public void identify() {
-        logger.info("Identifying "+this.toString());
+        logger.info("Identifying " + this.toString());
     }
 
     @Override
@@ -110,14 +160,17 @@ public class VirtualConsoleButton implements LightbulbAccessory {
         return CompletableFuture.completedFuture(firmwareRevision);
     }
 
+    public int getQlcId() {
+        return qlcId;
+    }
 
     @Override
     public String toString() {
         return "VirtualConsoleButton{" +
                 "accessoryId=" + accessoryId +
-                ", htmlId=" + htmlId +
+                ", qlcId=" + qlcId +
                 ", name='" + name + '\'' +
-                ", state=" + (state?"on":"off") +
+                ", state=" + (state.get() ? "ON" : "OFF") +
                 '}';
     }
 }
